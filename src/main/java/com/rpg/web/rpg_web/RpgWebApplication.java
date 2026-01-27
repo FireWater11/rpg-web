80666357fe95ac4f5c2d69ac2f66d9120570f6bc
Luidi Augusto <luidi.augusto1234@gmail.com>
2026-01-27 01:37:49 -0300

    feat: basic configuration

---
+++ src/main/java/com/rpg/web/rpg_web/RpgWebApplication.java
@@ -0,0 +1,13 @@
+package com.rpg.web.rpg_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgWebApplication.class, args);
+	}
+
+}
