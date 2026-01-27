2ed192b1341247a549bb1d990c5ab390a1e0700d
Luidi Augusto <luidi.augusto1234@gmail.com>
2026-01-27 02:51:43 -0300

    feat: creat base controller logic

--- src/main/java/com/rpg/web/rpg_web/controllers/ControllerCadastro.java
+++ src/main/java/com/rpg/web/rpg_web/controllers/ControllerCadastro.java
@@ -1,11 +1,18 @@
 package com.rpg.web.rpg_web.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.rpg.web.rpg_web.services.ServiceCadastro;
 
+@RestController
+@RequestMapping("/register")
 public class ControllerCadastro {
 
-    
+    private ServiceCadastro service;
+
+    public ControllerCadastro(ServiceCadastro service) {
+        this.service = service;
+    }
 
 }
