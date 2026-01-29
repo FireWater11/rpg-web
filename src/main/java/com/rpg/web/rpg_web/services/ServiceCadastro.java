92c08c7aef12f369ee53dd4b988ba40efbb10f0d
Luidi Augusto <luidi.augusto1234@gmail.com>
2026-01-29 04:36:04 -0300

    feat: verification if body is correct informations

--- src/main/java/com/rpg/web/rpg_web/services/ServiceCadastro.java
+++ src/main/java/com/rpg/web/rpg_web/services/ServiceCadastro.java
@@ -1,5 +1,22 @@
 package com.rpg.web.rpg_web.services;
 
+import org.springframework.http.ResponseEntity;
+
+import com.rpg.web.rpg_web.infra.dto.UserDTO;
+
 public class ServiceCadastro {
+
+    public ResponseEntity<String> register(UserDTO body) {
+        
+        if (body.getUsername() == null || body.getUsername().isBlank() || 
+        body.getEmail() == null || 
+        body.getEmail().isBlank() || 
+        body.getPassword() == null || body.getPassword().isBlank()) {
+            // Para retornar um Status HTTP: ResponseEntity.(nome do status).(se vai retornar algo no body)
+            return ResponseEntity.badRequest().body("Informações obrigatorias.");
+        }
+
+
+    }
     
 }
