af53f0f38e4d9ff5ec4c55ec88fae72c20c737d7
Luidi Augusto <luidi.augusto1234@gmail.com>
2026-01-30 23:47:30 -0300

    fix: Class referent fix

--- src/main/java/com/rpg/web/rpg_web/infra/repository/UsersRepository.java
+++ src/main/java/com/rpg/web/rpg_web/infra/repository/UsersRepository.java
@@ -4,8 +4,10 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.rpg.web.rpg_web.infra.domain.UsersDomain;
+
 
 // <Classe referente, tipo da PK>
-public interface UsersRepository extends JpaRepository<UsersRepository, UUID> {
+public interface UsersRepository extends JpaRepository<UsersDomain, UUID> {
     boolean existsByEmail(String email);
 }
