7b84d10aa085f895a6b454c7897083a932a0f599
Luidi Augusto <luidi.augusto1234@gmail.com>
2026-01-31 01:35:17 -0300

    fix: update UserRequestDTO

---
+++ src/main/java/com/rpg/web/rpg_web/infra/dto/UserRequestDTO.java
@@ -0,0 +1,13 @@
+package com.rpg.web.rpg_web.infra.dto;
+
+import lombok.Data;
+
+@Data
+
+public class UserRequestDTO {
+    private String username;
+
+    private String email;
+
+    private String password;
+}
